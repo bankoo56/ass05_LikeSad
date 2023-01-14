cb9ef5c43394d05a91f1420fc30a32a0b034b6e0
Pongsagon <bankoo525@gmail.com>
2023-01-14 14:23:47 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<script setup>
+import LikeSad from "./components/LikeSad.vue";
+</script>
+<style>
+body {
+  opacity: 100%;
+  background-image: url(https://i.ytimg.com/vi/R9mXtzn8meE/maxresdefault.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
+<template>
+  <div class="grid">
+    <div class="card">
+      <img
+        src="https://foyr.com/learn/wp-content/uploads/2021/08/design-your-dream-home.jpg"
+        alt=""
+        class="imgcenter"
+      />
+      <h4>Home</h4>
+      <p>detail</p>
+      <div>
+        <LikeSad> </LikeSad>
+      </div>
+    </div>
+
+    <div class="card">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNrZl9NTOPvF8kE3ia__xZ_RjSrnM_ls-dkQ&usqp=CAU"
+        alt=""
+        class="imgcenter"
+      />
+      <h4>Home</h4>
+      <p>detail</p>
+      <div>
+        <LikeSad> </LikeSad>
+      </div>
+    </div>
+
+    <div class="card">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWxE7DV9ATl_GlmAyc4ejIhy3OIrVGVX8fZ9bGuLi9UHN6wt8TQjra1lmrSySt20ThKW8&usqp=CAU"
+        alt=""
+        class="imgcenter"
+      />
+      <h4>Home</h4>
+      <p>detail</p>
+      <div>
+        <LikeSad> </LikeSad>
+      </div>
+    </div>
+
+    <div class="card">
+      <img
+        src="https://www.realestate.com.au/blog/images/2000x1500-fit,progressive/2021/11/24151024/Rawson_Facade2_2000x1500.jpg"
+        alt=""
+        class="imgcenter"
+      />
+      <h4>Home</h4>
+      <p>detail</p>
+      <div>
+        <LikeSad> </LikeSad>
+      </div>
+    </div>
+
+    <div class="card">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJQhxdTUh1pKA1sThcw1Jgg4lxUATDfTjU8gdYMNV2VOf1vcSD-p-DeckmGdQqusYYuG4&usqp=CAU"
+        alt=""
+        class="imgcenter"
+      />
+      <h4>Home</h4>
+      <p>detail</p>
+      <div>
+        <LikeSad> </LikeSad>
+      </div>
+    </div>
+
+    <div class="card">
+      <img
+        src="https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        alt=""
+        class="imgcenter"
+      />
+      <h4>Home</h4>
+      <p>detail</p>
+      <div>
+        <LikeSad> </LikeSad>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/LikeSad.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="margin: 3%; display: flex; gap: 10px; justify-content: end">
+    <button type="button" class="btn btn-secondary" @click="likeClick">
+      <i class="bi bi-emoji-smile"></i> Like {{ like }}
+    </button>
+    <button type="button" class="btn btn-secondary" @click="sadClick">
+      <i class="bi bi-emoji-frown"></i> Sad {{ sad }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const like = ref(0);
+const sad = ref(0);
+function likeClick() {
+  like.value++;
+}
+function sadClick() {
+  sad.value++;
+}
+</script>
+<script>
+export default {
+  name: "LikeSad",
+};
+</script>
